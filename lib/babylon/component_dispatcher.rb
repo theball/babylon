2ded1a3adfc64c37c11c902fe453a839a5e463c7
Astro <astro@spaceboyz.net>
2009-01-30 19:09:02 +0100

    component_dispatcher: add state awareness to not accidently react on bogus stream:stream stanzas

--- lib/babylon/component_dispatcher.rb
+++ lib/babylon/component_dispatcher.rb
@@ -1,7 +1,12 @@
 module Babylon
   class ComponentDispatcher < Dispatcher
+    def initialize(*a)
+      super
+      @state = :wait_for_stream
+    end
+
     def dispatch(stanza)
-      if stanza.name == "stream"
+      if @state == :wait_for_stream && stanza.name == "stream"
         if stanza.attributes['id']
           # This means the XMPP session started!
           # We must send the handshake now.
@@ -9,15 +14,18 @@ module Babylon
           handshake = REXML::Element.new("handshake")
           handshake.add_text(hash)
           send(handshake)
+          @state = :wait_for_handshake
         else
           # Weird!
         end
-      elsif stanza.name == "handshake"
+
+      elsif @state == :wait_for_handshake && stanza.name == "handshake"
         # Awesome, we're now connected and authentified, let's callback the controllers to tell them we're connected!
         @controllers.each do |controller|
           controller.on_connected
         end
-      else
+
+      elsif @state == :connected
         super # Can be dispatched
       end
     end
