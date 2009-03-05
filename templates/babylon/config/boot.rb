e93441db4511a2a4790f348c7897cf1099d1ceef
Nolan Darilek <nolan@thewordnerd.info>
2009-03-05 14:49:35 -0600

    Added support for custom initializers, no need to touch config/boot.rb.

--- templates/babylon/config/boot.rb
+++ templates/babylon/config/boot.rb
@@ -4,5 +4,8 @@ require "babylon"
 # Load the controllers
 Dir.glob(File.join(File.dirname(__FILE__), '../app/controllers/*_controller.rb')).each {|f| require f }
 
+# And the initializers, too.
+Dir.glob(File.join(File.dirname(__FILE__), '/initializers/*.rb')).each {|f| require f }
+
 # And start the App
 Babylon::Runner::run()
