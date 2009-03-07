6b70ecee9bf629b99e775b2644a4842e136fc294
Nolan Darilek <nolan@thewordnerd.info>
2009-03-07 09:43:59 -0600

    Fixed broken path to initializers in require.

--- templates/babylon/config/boot.rb
+++ templates/babylon/config/boot.rb
@@ -9,5 +9,5 @@ Dir.glob(File.join(File.dirname(__FILE__), '../app/controllers/*_controller.rb')
 # And start the App
 Babylon::Runner::run() {
   # And the initializers, too. This is done here since some initializers might need EventMachine to be started.
-  Dir.glob(File.join(File.dirname(__FILE__), '../initializers/*.rb')).each {|f| require f }
+  Dir.glob(File.join(File.dirname(__FILE__), '/initializers/*.rb')).each {|f| require f }
 }
