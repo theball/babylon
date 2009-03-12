b62ac31081d01423fd217471f938d233d1bad0fe
Julien Genestoux <julien.genestoux@gmail.com>
2009-03-12 08:38:23 -0700

    refactoring boot.rb and runner.rb to make it more 'logical'

--- lib/babylon/runner.rb
+++ lib/babylon/runner.rb
@@ -9,23 +9,29 @@ module Babylon
     # It then loads the models.
     # Finally it starts the EventMachine and connect the ComponentConnection
     # You can pass an additional block that will be called upon launching, when the eventmachine has been started.
-    def self.run(&callback) 
-      Babylon.config = YAML::load(File.new('config/config.yaml'))[BABYLON_ENV] 
-      #  Require routes defined with the new DSL router.
-      require "config/routes"
-      # Requiring all models
-      Dir.glob('app/models/*.rb').each { |f| require f }
-      
+    def self.run(&callback)       
       # Starting the EventMachine
       EventMachine.epoll
       EventMachine::run do
-        conn_opts = Babylon.config.merge({:on_stanza => CentralRouter.method(&:route)})
+        params = [Babylon.config.merge({:on_stanza => CentralRouter.method(&:route)}), CentralRouter.method(&:connected)] 
         case Babylon.config["application_type"]
           when "client"
-            Babylon::ClientConnection.connect(conn_opts, CentralRouter.method(&:connected))
+            Babylon::ClientConnection.connect(params)
           else # By default, we assume it's a component
-          Babylon::ComponentConnection.connect(conn_opts, CentralRouter.method(&:connected))
+          Babylon::ComponentConnection.connect(params)
         end
+        
+        # Requiring all models
+        Dir.glob('app/models/*.rb').each { |f| require f }
+
+        # Load the controllers
+        Dir.glob('app/controllers/*_controller.rb')).each {|f| require f }
+
+        Babylon.config = YAML::load(File.new('config/config.yaml'))[BABYLON_ENV] 
+
+        #  Require routes defined with the new DSL router.
+        require "config/routes"
+        
         # And finally, let's allow the application to do all it wants to do after we started the EventMachine!
         callback.call if callback
       end
